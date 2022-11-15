cb8a4d21618356792162c002ccc65ce5e93ac70c
Przemysław Król <pkr@unik.dk>
2022-11-15 16:14:40 +0100

    problems with data feed

--- Simple_CQRS_POC.Persistance/DatabaseInitializer/SampleDataInitilizer.cs
+++ Simple_CQRS_POC.Persistance/DatabaseInitializer/SampleDataInitilizer.cs
@@ -1,40 +1,39 @@
-﻿using Simple_CQRS_POC.Domain.Entities;
-using Simple_CQRS_POC.Domain.Enums;
+﻿//using Simple_CQRS_POC.Domain.Entities;
+//using Simple_CQRS_POC.Domain.Enums;
 
-namespace Simple_CQRS_POC.Persistance.DatabaseInitializer
-{
-    public static class SampleDataInitilizer
-    {
-
-        public static IEnumerable<Auction> DataFeed()
-        {
-            return new Auction[3]
-            {
-                new Auction("Opel Astra III",
-                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 1},
-                    "Michael Scott",
-                    DateTime.Now,
-                    DateTime.Now.AddDays(7),
-                    "Przedmiotem aukcji jest stary gruz opel astra",
-                    2000m,
-                    false) { Id = 1 },
-            new Auction("Opel Astra III",
-                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 2},
-                    "Michael Scott",
-                    DateTime.Now,
-                    DateTime.Now.AddDays(7),
-                    "Przedmiotem aukcji jest stary gruz opel astra",
-                    2000m,
-                    false) { Id = 2 },
-            new Auction("Opel Astra III",
-                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 3},
-                    "Michael Scott",
-                    DateTime.Now,
-                    DateTime.Now.AddDays(7),
-                    "Przedmiotem aukcji jest stary gruz opel astra",
-                    2000m,
-                    false) { Id = 3 }
-        };
-        }
-    }
-}
+//namespace Simple_CQRS_POC.Persistance.DatabaseInitializer
+//{
+//    public static class SampleDataInitilizer
+//    {
+//        public static IEnumerable<Auction> DataFeed()
+//        {
+//            return new Auction[3]
+//            {
+//                new Auction("Opel Astra III",
+//                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 1},
+//                    "Michael Scott",
+//                    DateTime.Now,
+//                    DateTime.Now.AddDays(7),
+//                    "Przedmiotem aukcji jest stary gruz opel astra",
+//                    2000m,
+//                    false) { Id = 1 },
+//            new Auction("Opel Astra III",
+//                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 2},
+//                    "Michael Scott",
+//                    DateTime.Now,
+//                    DateTime.Now.AddDays(7),
+//                    "Przedmiotem aukcji jest stary gruz opel astra",
+//                    2000m,
+//                    false) { Id = 2 },
+//            new Auction("Opel Astra III",
+//                    new Item("Opel Astra III 1.5 LPG", "Nice car", Category.CARS, false) { AuctionId = 3},
+//                    "Michael Scott",
+//                    DateTime.Now,
+//                    DateTime.Now.AddDays(7),
+//                    "Przedmiotem aukcji jest stary gruz opel astra",
+//                    2000m,
+//                    false) { Id = 3 }
+//        };
+//        }
+//    }
+//}
